33419bc3b825143245afbb0faf10236959b2dc8a
Cristian Luna <cluna-2023308@kinal.edu.gt>
2025-02-25 23:44:48 -0600

    modelo de usuario terminado

--- configs/server.js
+++ configs/server.js
@@ -1,7 +1,27 @@
 "use strict"
 
 import express from "express"
+import cors from "cors"
+import helmet from "helmet"
+import morgan from "morgan"
 import { dbConnection } from "./mongo.js"
+import authRoutes from "../src/auth/auth.routes.js" 
+import apiLimiter from "../src/middlewares/rate-limit-validators.js"
+import createAdminUser from "../src/auth/auth.controller.js"
+
+const middlewares = (app) => {
+    app.use(express.urlencoded({extended: false}))
+    app.use(express.json())
+    app.use(cors());
+    app.use(helmet());
+    app.use(morgan("dev"))
+    app.use(apiLimiter)
+}
+
+const routes = (app) =>{
+    app.use("/opinionSystem/v1/auth", authRoutes)
+}
+
 
 const conectarDB = async () => {
     try{
@@ -14,7 +34,10 @@ const conectarDB = async () => {
 export const initServer = () => {
     const app = express()
     try{
+        createAdminUser()
+        middlewares(app)
         conectarDB()
+        routes(app)
         app.listen(process.env.PORT)
         console.log(`Server running on port ${process.env.PORT}`)
     }catch(err){
---
+++ src/auth/auth.controller.js
@@ -0,0 +1,96 @@
+import { hash, verify } from "argon2"
+import User from "../user/user.model.js"
+
+export const register = async (req, res) => {
+    try {
+        const data = req.body;
+        let profilePicture = req.file ? req.file.filename : null;
+        const encryptedPassword = await hash(data.password)
+        data.password = encryptedPassword
+        data.profilePicture = profilePicture
+
+        const user = await User.create(data);
+
+        return res.status(201).json({
+            message: "User has been created",
+            name: user.name,
+            email: user.email
+        });
+    } catch (err) {
+        return res.status(500).json({
+            message: "User registration failed",
+            error: err.message
+        });
+    }
+}
+
+export const login = async (req, res) => {
+    const { email, username, password } = req.body
+    try{
+        const user = await User.findOne({
+            $or:[{email: email}, {username: username}]
+        })
+
+        if(!user){
+            return res.status(400).json({
+                message: "Crendenciales inválidas",
+                error:"No existe el usuario o correo ingresado"
+            })
+        }
+
+        const validPassword = await verify(user.password, password)
+
+        if(!validPassword){
+            return res.status(400).json({
+                message: "Crendenciales inválidas",
+                error: "Contraseña incorrecta"
+            })
+        }
+
+        const token = await generateJWT(user.id)
+
+        return res.status(200).json({
+            message: "Login successful",
+            userDetails: {
+                token: token,
+                profilePicture: user.profilePicture
+            }
+        })
+    }catch(err){
+        return res.status(500).json({
+            message: "login failed, server error",
+            error: err.message
+        })
+    }
+}
+
+const createAdminUser = async () => {
+    try {
+  
+      const adminExists = await User.findOne({ role: "ADMIN_ROLE" });
+  
+      if (adminExists) {
+        console.log("El superadmin ya existe, no se puede crear otro");
+        return;
+      }
+  
+      const hashedPassword = await hash("Admin123-");
+  
+      const superAdmin = new User({
+        name: "Super",
+        surname: "Admin",
+        username: "SuperAdmin",
+        email: "superadmin@gmail.com",
+        password: hashedPassword,
+        profilePicture: null,
+        role: "ADMIN_ROLE"
+      });
+  
+      await superAdmin.save();
+      console.log("Superadmin creado correctamente.");
+    } catch (error) {
+      console.error("Error al verificar o crear el superadmin:", error.message);
+    }
+  };
+  
+  export default createAdminUser;
